0fa07054d99109c9745db0dd1337cad52258a887
sookim <sookim@student.42seoul.kr>
2020-07-07 13:19:52 +0900

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sookim <sookim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/07 13:17:38 by sookim            #+#    #+#             */
+/*   Updated: 2020/07/07 13:17:39 by sookim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
+
+int				get_next_line(int fd, char **line);
+
+size_t			ft_strlen(const char *s);
+size_t			ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t			ft_strlcat(char *dest, const char *src, size_t dstsize);
+char			*ft_strdup(char *s1);
+char			*ft_strjoin(char *s1, char *s2);
+
+#endif
