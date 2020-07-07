272bc1ad0761b9dd219c5783e43f346d37c3cf9c
sookim <sookim@student.42seoul.kr>
2020-07-07 17:34:25 +0900

    final

--- get_next_line.c
+++ get_next_line.c
@@ -6,58 +6,58 @@
 /*   By: sookim <sookim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 15:31:47 by sookim            #+#    #+#             */
-/*   Updated: 2020/07/07 16:45:36 by sookim           ###   ########.fr       */
+/*   Updated: 2020/07/07 17:32:49 by sookim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int					is_newline(char *backup)
+int					ft_new_line_check(char *temp_buf)
 {
 	int				i;
 
 	i = 0;
-	while (backup[i])
+	while (temp_buf[i])
 	{
-		if (backup[i] == '\n')
+		if (temp_buf[i] == '\n')
 			return (i);
 		i++;
 	}
 	return (-1);
 }
 
-int					split_line(char **backup, char **line, int cut_idx)
+int					ft_line_split(char **temp_buf, char **line, int new_line_idx)
 {
-	char			*temp;
+	char			*new_temp_buf;
 	int				len;
 
-	(*backup)[cut_idx] = '\0';
-	*line = ft_strdup(*backup);
-	len = ft_strlen(*backup + cut_idx + 1);
+	(*temp_buf)[new_line_idx] = '\0';
+	*line = ft_strdup(*temp_buf);
+	len = ft_strlen(*temp_buf + new_line_idx + 1);
 	if (len == 0)
 	{
-		free(*backup);
-		*backup = 0;
+		free(*temp_buf);
+		*temp_buf = 0;
 		return (1);
 	}
-	temp = ft_strdup(*backup + cut_idx + 1);
-	free(*backup);
-	*backup = temp;
+	new_temp_buf = ft_strdup(*temp_buf + new_line_idx + 1);
+	free(*temp_buf);
+	*temp_buf = new_temp_buf;
 	return (1);
 }
 
-int					return_all(char **backup, char **line, int read_size)
+int					return_all(char **temp_buf, char **line, int read_size)
 {
-	int				cut_idx;
+	int				new_line_idx;
 
 	if (read_size < 0)
 		return (-1);
-	if (*backup && (cut_idx = is_newline(*backup)) >= 0)
-		return (split_line(backup, line, cut_idx));
-	else if (*backup)
+	if (*temp_buf && (new_line_idx = ft_new_line_check(*temp_buf)) >= 0)
+		return (ft_line_split(temp_buf, line, new_line_idx));
+	else if (*temp_buf)
 	{
-		*line = *backup;
-		*backup = 0;
+		*line = *temp_buf;
+		*temp_buf = 0;
 		return (0);
 	}
 	*line = ft_strdup("");
@@ -66,19 +66,19 @@ int					return_all(char **backup, char **line, int read_size)
 
 int					get_next_line(int fd, char **line)
 {
-	static char		*backup[OPEN_MAX];
+	static char		*temp_buf[OPEN_MAX];
 	char			buf[BUFFER_SIZE + 1];
 	int				read_size;
-	int				cut_idx;
+	int				new_line_idx;
 
-	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0))
+	if ((fd < 0) || (line == NULL) || (BUFFER_SIZE < 1))
 		return (-1);
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[read_size] = '\0';
-		backup[fd] = ft_strjoin(backup[fd], buf);
-		if ((cut_idx = is_newline(backup[fd])) >= 0)
-			return (split_line(&backup[fd], line, cut_idx));
+		temp_buf[fd] = ft_strjoin(temp_buf[fd], buf);
+		if ((new_line_idx = ft_new_line_check(temp_buf[fd])) >= 0)
+			return (ft_line_split(&temp_buf[fd], line, new_line_idx));
 	}
-	return (return_all(&backup[fd], line, read_size));
+	return (return_all(&temp_buf[fd], line, read_size));
 }
